b0eab8622e477ed14077153720210d4bc62a86cb
Ozgur Karakus <ozgurkrks0697@gmail.com>
2024-06-14 15:37:25 +0300

    add: copy link

--- src/pages/Links/Links.jsx
+++ src/pages/Links/Links.jsx
@@ -9,12 +9,16 @@ import styles from "./Links.module.css";
 import { useOutletContext } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress";
 import useAxios from "../../hooks/useAxios";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { toast } from "react-toastify";
 
 const API_URL = process.env.REACT_APP_API_URL + "links/";
 
 export default function Links() {
   const { user } = useSelector((state) => state.auth);
+  const { userData } = useSelector((state) => state.user);
   const [load, setLoad] = useState(false);
+  const [copy, setCopy] = useState(null);
   const { put } = useAxios();
 
   const dispatch = useDispatch();
@@ -30,6 +34,10 @@ export default function Links() {
     dispatch(updateLinksDataHandler(someProp.items));
   }, [someProp.items]);
 
+  useEffect(() => {
+    copy && toast.success("Copied");
+  }, [copy]);
+
   return (
     <div
       style={{
@@ -41,6 +49,50 @@ export default function Links() {
         scrollbarGutter: "auto",
       }}
     >
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "30px",
+        }}
+      >
+        <div
+          style={{
+            width: "90%",
+            display: "flex",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+            backgroundColor: "#A4DDED",
+            padding: "5px",
+            borderRadius: "20px",
+          }}
+        >
+          <div style={{ padding: "8px", fontSize: "13px" }}>
+            <strong>🔥 Your link is live: </strong> linkgo-front.vercel.app/
+            {userData.username}
+          </div>
+          <CopyToClipboard
+            text={`https://linkgo-front.vercel.app/${userData.username}`}
+            onCopy={() => setCopy(true)}
+          >
+            <button
+              style={{
+                border: "none",
+                borderRadius: "30px",
+                backgroundColor: "white",
+                color: "black",
+                width: "150px",
+                padding: "8px",
+                cursor: "pointer",
+                fontWeight: 600,
+              }}
+            >
+              Copy your link
+            </button>
+          </CopyToClipboard>
+        </div>
+      </div>
       <div
         style={{
           width: "100%",
